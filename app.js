3ef1c7610d9489be15e9e4361f399d9f34fb9ea2
sharmakavya281105 <kavyarahul9610@gmail.com>
2021-12-06 22:14:51 +0530

    Login page created successfully

--- app.js
+++ app.js
@@ -5,87 +5,96 @@ const bodyparser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser:true,useUnifiedTopology:true})
+// mongoose.connect('mongodb://localhost:27017/retailerDB',{useNewUrlParser:true,useUnifiedTopology:true})
+// mongoose.set('useCreateIndex',true)
+// secret = process.env.SECRET || "hello"
 
 const app = express()
 
-userSchema = new mongoose.schema({
+retailerSchema = new mongoose.Schema({
+  name:String,
   email:String,
   password:String
 })
 
-secret = process.env.SECRET
-userSchema.plugin(encrypt,{secret:secret,encryptedField:['password']})
+userSchema.plugin(passportLocalMongoose)
 
-const User = mongoose.model('User',userSchema)
+const Retailer = mongoose.model('Retailer',retailerSchema)
 
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use(session({
+  secret:'ourSecret',
+  resave:false,
+  saveUninitialized:true
+}))
 
+app.use(passport.initialize())
+app.use(passport.session())
 app.set('view engine','ejs');
 
 
-app.get('/',function(req,res){
-  res.render('home')
-})
-app.get('/register',function(req,res){
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+app.get('/register',(req,res)=>{
   res.render('register')
 })
 
-app.post('/register',function(req,res){
-  const user = new User({
-    email    : req.body.username,
-    password : req.body.password
-  })
-  user.save(function(err){
-    if(!err){
-      res.render('secrets')
-    }
-    else {
-      res.send(err)
-    }
-  })
-
-})
-
-app.get('/login',function(req,res){
+app.get('/login',(req,res)=>{
   res.render('login')
 })
 
-app.post('/login',function(req,res){
-  email = req.body.username
-  password = req.body.password
-
-  User.findOne({email:email},function(err,foundUser){
-
-      if(foundUser.password === password){
-        res.render('secrets')
-      }
-      else {
-        res.send('Unknown username or password')
-      }
-
+app.post('/register',function(req,res){
+  Retailer.register({email:req.body.email,username:req.body.username},req.body.password,(req,res)=>{
+    if(err){
+      console.log(err);
+      res.redirect('/register')
+    }
+    else {
+      passport.authenticate('local')(req,res,()=>{
+        res.redirect('/dashboard')
       })
 
+    }
+  })
 
 })
 
-
-
-app.get('/secrets',function(req,res){
-  res.render('secrets')
+app.post('login',(req,res)=>{
+  const retailer = new Retailer({
+    email:req.body.email,
+    password:req.body.password
+  })
+  req.login(user,(err)=>{
+    if(err){
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req,res,function(){
+        res.redirect('dashboard')
+      })
+    }
+  })
 })
 
-app.get('/submit',function(req,res){
-  res.render('submit')
+
+app.get('/dashboard',function(req,res){
+  if(req.isAuthenticated()){
+    res.render('dashboard')
+  }
+  else {
+    res.render('login')
+  }
 })
-
-
-
-
-
 
 
 app.listen(3000,function(){
