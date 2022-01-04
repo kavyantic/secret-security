8c130a1f0085c79e576e0efef6bccf826e0a133d
sharmakavya281105 <kavyarahul9610@gmail.com>
2022-01-04 15:55:25 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var router = require('express').Router();
-
+router.get('/',(req,res)=>{
+    res.redirect('/login')
+})
 router.use('/login', require('./login'));
 router.use('/admin',require('./admin'))
 router.use('/superDistributor',require('./superDistributor'))
