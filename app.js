bae51aef5e3806423695e5206f5db6851ec626b4
sharmakavya281105 <kavyarahul9610@gmail.com>
2022-02-24 13:20:15 +0530

    main

--- app.js
+++ app.js
@@ -14,10 +14,8 @@ const autoIncrement = require('mongoose-auto-increment');
 const fileUpload = require('express-fileupload');
 process.env.serviceTime = "23:59"
 
-var isProduction = process.env.NODE_ENV === 'production';
 
-process.env.LOCAL_DB_URL = "mongodb://localhost:27017/rechargePortal"
-mongoose.connect(process.env.LOCAL_DB_URL || "mongodb+srv://rahulkavya9610:painter05@cluster0.afuye.mongodb.net/rechargePortal?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect("mongodb+srv://rahulkavya9610:painter05@cluster0.afuye.mongodb.net/rechargePortal?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
 mongoose.set('useCreateIndex',true)
 autoIncrement.initialize(mongoose);
 
